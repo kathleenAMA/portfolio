6beeed83a040f11b96fd31bbfdedc156fd461618
Kathleen Robertson <kroberrtsonn@gmail.com>
2017-02-23 17:11:20 -0700

    updating design

--- js/js.js
+++ js/js.js
@@ -1,103 +1,33 @@
-jQuery(document).ready(function ($) {
+
+  $(document).ready(function() {
+
+      var windw = this;
+
+      $.fn.followTo = function ( pos , pos2 ) {
+          var $this = this,
+              $window = $(windw);
+
+          $window.scroll(function(e){
+              if ($window.scrollTop() > pos && $window.scrollTop() < pos2) {
+                  $this.css({
+                      position: 'fixed',
+                      top: 0
+                  });
+              } else if ($window.scrollTop() < pos) {
+                  $this.css({
+                      position: 'relative',
+                      top: 0
+                  });
+              } else if ($window.scrollTop() > pos2) {
+                $this.css({
+                    position: 'relative',
+                    top: 50
+                });
+              }
+
+          });
+      };
 
 
-    //initialise Stellar.js
-    $(window).stellar();
-
-    //Cache some variables
-    var links = $('.navigation').find('li');
-    slide = $('.slide');
-    button = $('.button');
-    mywindow = $(window);
-    htmlbody = $('html,body');
-
-
-    //Setup waypoints plugin
-    slide.waypoint(function (event, direction) {
-
-        //cache the variable of the data-slide attribute associated with each slide
-        dataslide = $(this).attr('data-slide');
-
-        //If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and 
-        //remove the active class from the previous navigation link 
-        if (direction === 'down') {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
-        }
-        // else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and 
-        //remove the active class from the next navigation link 
-        else {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
-        }
-
-    });
-
-    //waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class 
-    //from navigation link slide 2 and adds it to navigation link slide 1. 
-    mywindow.scroll(function () {
-        if (mywindow.scrollTop() == 0) {
-            $('.navigation li[data-slide="1"]').addClass('active');
-            $('.navigation li[data-slide="2"]').removeClass('active');
-        }
-    });
-
-    //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
-    //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
-    function goToByScroll(dataslide) {
-        htmlbody.animate({
-            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
-        }, 2000, 'easeInOutQuint');
-    }
-
-
-
-    //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
-    links.click(function (e) {
-        e.preventDefault();
-        dataslide = $(this).attr('data-slide');
-        goToByScroll(dataslide);
-    });
-
-    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
-    button.click(function (e) {
-        e.preventDefault();
-        dataslide = $(this).attr('data-slide');
-        goToByScroll(dataslide);
-
-    });
-
-    $(function() {
-var fixadent = $("#slide2"), pos = fixadent.offset();
-$(window).scroll(function() {
-if($(this).scrollTop() > (pos.top) && fixadent.css('position') == 'static') { fixadent.addClass('fixed'); }
-else if($(this).scrollTop() <= pos.top && fixadent.hasClass('fixed')){ fixadent.removeClass('fixed'); }
-})
-});
-
-        $(function() {
-var fixadent = $("#slide3"), pos = fixadent.offset();
-$(window).scroll(function() {
-if($(this).scrollTop() > (pos.top) && fixadent.css('position') == 'relative') { fixadent.addClass('fixed'); }
-else if($(this).scrollTop() <= pos.top && fixadent.hasClass('fixed')){ fixadent.removeClass('fixed'); }
-})
-});
-
-
-        var bodyHeight = $("body").height()-$(window).height();
-window.onscroll = function() {
-    var deg = -$(window).scrollTop()*(8440/bodyHeight);
-    $(".logo").css({
-        "transform": "rotate("+deg+"deg)",
-    });
-    
-      var deg = -$(window).scrollTop()*(-8440/bodyHeight);
-     $(".logo2").css({
-        "transform": "rotate("+deg+"deg)",
-    });
-
-
-};
-
-
-
-});
-
+      $('#fixme').followTo(1800, 5500);
+  });
